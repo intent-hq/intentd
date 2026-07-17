c2a91c067623ea290609087e25ceba85f536c4b1
Clement Pang <panghy@users.noreply.github.com>
2026-07-18 00:49:51 +0800

    chore: run cargo fmt

--- crates/intent-transport/src/lifecycle.rs
+++ crates/intent-transport/src/lifecycle.rs
@@ -115,10 +115,7 @@ impl WsInner {
         }
         match TcpListener::bind((self.bind_address, self.base_port)).await {
             Ok(listener) => {
-                let port = listener
-                    .local_addr()
-                    .map_err(Arc::new)?
-                    .port();
+                let port = listener.local_addr().map_err(Arc::new)?.port();
                 Ok((listener, port))
             }
             Err(e) => Err(Arc::new(e)),
